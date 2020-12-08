45db1eb7a12fb2ad6ddd23767d5d6d0e27d893ee
Shivam Verma <vermashivam527@gmail.com>
2020-12-09 00:01:53 +0530

    functionality done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import resourceData from "./appData";
 
 export default function App() {
+  const [resourceType, setResourceType] = useState("youtube");
+
+let resourceList = resourceData[resourceType];
+
+  const resourceTypeArray = Object.keys(resourceData);
+
+  function resourceTypeChange(type) {
+    setResourceType(type);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>JavaScript</h1>
+      <h2>Free Learning Resources</h2>
+      {resourceTypeArray.map((type) => {
+        return <button onClick={() => resourceTypeChange(type)}>{type}</button>;
+      })}
+      {resourceList.map((data) => {
+        return (
+          <div>
+            <p>{data.name}</p>
+            <a href={data.link}>Link</a>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/appData.js
@@ -0,0 +1,60 @@
+const resourceData = {
+  youtube: [
+    {
+      name: "Tanay Pratap",
+      link: "https://www.youtube.com/tanaypratap"
+    },
+    {
+      name: "Akshay Saini",
+      link: "https://www.youtube.com/channel/UC3N9i_KvKZYP4F84FPIzgPQ"
+    },
+    {
+      name: "Traversy Media",
+      link: "https://www.youtube.com/user/TechGuyWeb"
+    },
+  ],
+  websites: [
+    {
+      name: "MDN Docs",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+    },
+    {
+      name: "freeCodeCamp",
+      link: "https://www.freecodecamp.org/"
+    },
+    {
+      name: "javascript.info",
+      link: "https://javascript.info/"
+    }
+  ],
+  Books: [
+    {
+      name: "Eloquent JavaScript",
+      link: "https://eloquentjavascript.net/Eloquent_JavaScript.pdf"
+    },
+    {
+      name: "You Don't Know JS",
+      link: "https://github.com/getify/You-Dont-Know-JS"
+    }
+  ],
+  Instagram: [
+    {
+      name: "Tanay Pratap",
+      link: "https://www.instagram.com/tanaypratap/"
+    },
+    {
+      name: "neogcamp",
+      link: "https://www.instagram.com/neogcamp/"
+    },
+    {
+      name: "JS Interview",
+      link: "https://www.instagram.com/javascript_interviews/"
+    },
+    {
+      name: "Javascript Essentials",
+      link: "https://www.instagram.com/javascriptessentials/"
+    }
+  ]
+};
+
+export default resourceData;
